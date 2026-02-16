01a9b7479432c8fab06461fa89cbe192f020e94e
donnie88c <cs_neeko@yahoo.com>
2026-02-16 10:35:49 +0200

    Remove unused files and code related to the Netflix clone project, including constants, API requests, views, styles, and images. This cleanup prepares the project for a fresh start or a new direction.

--- src/common/constants.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = 'c5607c4eefbb691f069a40b34ce2510d';
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const YT_BASE = 'https://www.youtube.com/embed/'; 
